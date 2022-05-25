adf0ea39e2447d257fa4e65202cf64bcbc969767
Andres Felipe Vargas <pipevarg1230@gmail.com>
2022-05-25 18:36:58 -0500

    .

---
+++ Fase 4 - Temas avanzados/Tema 14 - Bases de datos con SQLite/Leccion3.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conexion=sqlite3.connect('usuarios_autoincremental.db')
+cursor=conexion.cursor()
+
+cursor.execute("SELECT * FROM usuarios WHERE id=1")
+
+usuario = cursor.fetchone()
+print(usuario)
+
+
+
+conexion.commit()
+conexion.close()
--- Fase 4 - Temas avanzados/Tema 14 - Bases de datos con SQLite/leccion2.py
+++ Fase 4 - Temas avanzados/Tema 14 - Bases de datos con SQLite/leccion2.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-conexion=sqlite3.connect('usuarios.db')
+conexion=sqlite3.connect('usuarios_autoincremental.db')
 cursor=conexion.cursor()
 
 #region comentario
@@ -29,14 +29,51 @@ cursor=conexion.cursor()
 
 
 
-cursor.execute('''
-	CREATE TABLE productos(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		nombre VARCHAR(100) NOT NULL,
-		marca VARCHAR(100) NOT NULL,
-		precio FLOAT NOT NULL
-	)
-	''')
+# cursor.execute('''
+# 	CREATE TABLE productos(
+# 		id INTEGER PRIMARY KEY AUTOINCREMENT,
+# 		nombre VARCHAR(100) NOT NULL,
+# 		marca VARCHAR(100) NOT NULL,
+# 		precio FLOAT NOT NULL
+# 	)
+# 	''')
+
+# productos=[
+# 	('Teclado','Logitech',19.95),
+# 	('Pantalla 19"','LG',19.95),
+# 	('Mouse','Logitech',19.95)
+
+# ]
+
+# cursor.executemany('INSERT INTO productos VALUES (null,?,?,?)',productos)
+
+
+# cursor.execute('SELECT * FROM productos')
+
+# productos=cursor.fetchall()
+# for producto in productos:
+# 	print(producto)
+
+
+# cursor.execute('''
+# 	CREATE TABLE usuarios(
+# 		id INTEGER PRIMARY KEY AUTOINCREMENT,
+# 		dn1 VARCHAR(9) UNIQUE,
+# 		nombr VARCHAR(100),
+# 		edad INTEGER,
+# 		email VARCHAR(100)
+# 	)
+# 	''')
+
+
+usuarios=[		('11111111A','Mario',54,'mario@ejemplo.com'),
+				('22222222B','Luis',30,'luis@ejemplo.com'),
+				('3peoridus','Sergio',20,'sergio@ejemplo.com'),
+				('4peoridus','Hector',25,'hector@ejemplo.com'),]
+
+
+cursor.executemany('INSERT INTO usuarios VALUES (null,?,?,?,?)',usuarios)
+
 
 conexion.commit()
 conexion.close()
